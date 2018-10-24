434f01f61eff9a5a535f4216050c401bdf1538d2
Mpho KGOSISEJO <mkgosise@e4r9p11.wethinkcode.co.za>
2018-10-24 14:19:17 +0200

    react app (client app) can now run on the same port as loopback (api)

---
+++ client/_next/static/ZbsU~mWwwonN_a~FVaFNB/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{436:function(t,e,n){__NEXT_REGISTER_PAGE("/about",function(){return t.exports=n(437),{page:t.exports.default}})},437:function(t,e,n){"use strict";n.r(e);var u=n(0),a=n.n(u),o=n(58);e.default=function(){return a.a.createElement(o.a,{title:"About"},a.a.createElement("p",null,"About page..."))}}},[[436,1,0]]]);
